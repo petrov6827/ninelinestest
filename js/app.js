4a314a86ff0cb7fe7875c7981df2ab456d1e5595
Dmitry <petrov6827@gmail.com>
2020-06-18 21:13:11 +0300

    scss, html, gulp

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+  var rangeMeter = document.querySelector('#range');
+  var rangeShow = document.querySelector("#show");
+  var rangeClock =  document.querySelector('.meter-clock');
+
+  function rangeChange() {
+    var rotateClock = rangeMeter.value;
+
+    rangeClock.style.transform = 'rotate(' + (-90 + ((rotateClock * 180) / 100)) + 'deg)';
+    rangeShow.value = rotateClock + '%';
+  }
+
+	rangeMeter.addEventListener('input', rangeChange);
